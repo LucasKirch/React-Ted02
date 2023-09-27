2fc6f635021b90ab9061c03295f5f4cbebb1cd52
Lucas Kirchesch <lucas@btor.com.br>
2023-09-27 20:43:13 -0300

    ted02

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,9 @@
 import PrimeiroComponente from "./Componentes/Basicos/Primeiro";
 import SegundoComponente from "./Componentes/Basicos/Segundo"
 import TerceiroComponente from "./Componentes/Basicos/Terceiro"
+import ArrowFunction from "./Componentes/Basicos/ArrowFunction";
+import Spred from "./Componentes/Basicos/Spread";
+import Ted02 from "./Componentes/Basicos/Ted02";
 
 function App() {
     return(
@@ -9,6 +12,9 @@ function App() {
         <PrimeiroComponente/>
         <SegundoComponente/>
         <TerceiroComponente texto1="Utilizando props" texto2="Continuação" />
+        <ArrowFunction um={0} dois={9} />
+        <Spred />
+        <Ted02  h1="NOME 1" h2="NOME 2"/>
     </div>
     )
 }
